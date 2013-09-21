65ee54a753eee670cf22f30c3b5cf019e3d879ad
cherniag <gennadiy.chernyaev@gmail.com>
2013-09-21 21:46:03 +0300

    Hibernate Tutorial 31 - Query By Example 	http://www.youtube.com/watch?v=2DXjdl8gzOo

--- src/main/java/com/gc/test/hibernate/Test.java
+++ src/main/java/com/gc/test/hibernate/Test.java
@@ -6,8 +6,7 @@ import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Example;
 
 import com.gc.test.hibernate.domain.UserDetails;
 
@@ -17,31 +16,24 @@ public class Test {
 			.buildSessionFactory();
 
 	public static void main(String[] args) throws IOException {
-
 		Session s /*
 				 * = sf.openSession(); s.beginTransaction(); for(int
 				 * i=0;i<10;i++){ UserDetails user = new
 				 * UserDetails().setUserName("user" + i); s.save(user); }
 				 * s.getTransaction().commit(); s.close()
 				 */;
-
 		s = sf.openSession();
 		s.beginTransaction();
-		Criteria criteria = s.createCriteria(UserDetails.class); // = " from UserDetails "
-		/*
-		 		Projections.max("userId")
-		 		Projections.count("userId")
-		 		Projections.property("userId")  = select userId from UserDetails
-		 		Projections.property("userId")
-		 				   .setOrder(Order.desc("userId")); = select userId from UserDetails order by userId desc
+		
+		UserDetails exampleUserDetails = new UserDetails();
+		/* 
+		 	exampleUserDetails.setUserId(5) - Hibernate ignores @Id and null values
+		 	exampleUserDetails.setUserName("user1%") = LIKE 'user1%' //need Example.create(...).enableLike();
 		 */
-		criteria.setProjection(Projections.property("userId"))
-				.addOrder(Order.desc("userId")); // Projections.max("userId")
-		
-		/*criteria.add(like("userName", "user%"))
-				  .add(gt("userId", 6));     // = " where userName like 'user%' AND userId > 6"
-		*/
-		
+		exampleUserDetails.setUserName("user1%"); 
+		Example example = Example.create(exampleUserDetails).enableLike();
+		Criteria criteria = s.createCriteria(UserDetails.class);
+		criteria.add(example);
 		
 		System.out.println(criteria.list());
 		s.getTransaction().commit();
